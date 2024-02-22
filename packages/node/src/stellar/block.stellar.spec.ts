13b675641d1ebf87f1af3c906ff1c3e506299fd2
JQQQ <jiqiang90@hotmail.com>
2024-02-23 10:32:30 +1300

    fix tests

--- packages/node/src/stellar/block.stellar.spec.ts
+++ packages/node/src/stellar/block.stellar.spec.ts
@@ -191,7 +191,6 @@ describe('StellarBlockWrapped', () => {
 
     const mockEventFilterValid: SorobanEventFilter = {
       topics: ['topic1', 'topic2'],
-      contractId: new Contract(testAddress).toString(),
     };
 
     const mockEventFilterInvalid: SorobanEventFilter = {
