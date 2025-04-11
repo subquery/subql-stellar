55c651cab9dfeac8ddd93f3776bf14d547e53203
Tate <yongzhao.tan@onfinality.io>
2025-04-11 03:38:20 +0000

    add global stellarClient

--- packages/types/src/global.ts
+++ packages/types/src/global.ts
@@ -1,10 +1,10 @@
 // Copyright 2020-2025 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0
 
-import {rpc} from '@stellar/stellar-sdk';
+import {Horizon, rpc} from '@stellar/stellar-sdk';
 import '@subql/types-core/dist/global';
 
 declare global {
   const api: undefined;
-  const unsafeApi: {sorobanClient: rpc.Server};
+  const unsafeApi: {sorobanClient: rpc.Server; stellarClient: Horizon.Server};
 }
